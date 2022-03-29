40d3a1efef15fc4e9f7765bb12c7cc18afffdeb1
KIBORG04 <bossmira4@gmail.com>
2022-03-29 18:43:57 +0300

    KeyMap struct for output control

--- internal/service/stats/stats.go
+++ internal/service/stats/stats.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/exp/slices"
+	"sort"
 	"ssstatistics/internal/domain"
+	"ssstatistics/internal/keymap"
 	r "ssstatistics/internal/repository"
 	"ssstatistics/internal/service/charts"
 	"ssstatistics/internal/utils"
@@ -18,13 +20,13 @@ func Root(c *gin.Context) {
 	str := fmt.Sprintf("%%%d%%", roots[len(roots)-1].RoundID)
 	r.Database.Where("link LIKE ?", str).First(&links)
 
-	crewDeathsCount := make(map[string]uint, 0)
+	crewDeathsCount := make(keymap.MyMap[string, uint], 0)
 	crewDeathsSum := 0
 
-	roleDeathsCount := make(map[string]uint, 0)
+	roleDeathsCount := make(keymap.MyMap[string, uint], 0)
 	roleDeathsSum := 0
 
-	modesCount := make(map[string]uint, 0)
+	modesCount := make(keymap.MyMap[string, uint], 0)
 	modesSum := 0
 
 	var alphaRoots []domain.Root
@@ -40,20 +42,24 @@ func Root(c *gin.Context) {
 			gammaRoots = append(gammaRoots, root)
 		}
 
-		modesCount[root.Mode]++
+		modesCount = keymap.AddElem(modesCount, root.Mode, 1)
 		modesSum++
 
 		for _, death := range root.Deaths {
 			if slices.Contains(stationPositions, death.AssignedRole) && utils.IsDrone.FindString(death.Name) == "" {
-				crewDeathsCount[death.AssignedRole]++
+				crewDeathsCount = keymap.AddElem(crewDeathsCount, death.AssignedRole, 1)
 				crewDeathsSum++
 			}
 			if death.SpecialRole != "" {
-				roleDeathsCount[death.SpecialRole]++
+				roleDeathsCount = keymap.AddElem(roleDeathsCount, death.SpecialRole, 1)
 				roleDeathsSum++
 			}
 		}
 	}
+
+	sort.Stable(sort.Reverse(modesCount))
+	sort.Stable(sort.Reverse(crewDeathsCount))
+	sort.Stable(sort.Reverse(roleDeathsCount))
 
 	c.HTML(200, "index.html", gin.H{
 		"totalRounds":     len(roots),
